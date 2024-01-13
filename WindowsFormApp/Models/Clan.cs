fda819fe56933398b52656fe8fc5b31b1951619b
sbicak20 <sbicak20@foi.hr>
2024-01-13 16:57:26 +0100

    a

---
+++ WindowsFormApp/Models/Clan.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime;
+
+namespace Models
+{
+    public class Clan
+    {
+        public required string Ime { get; set; }
+        public required string Prezime { get; set; }
+    }
+}
